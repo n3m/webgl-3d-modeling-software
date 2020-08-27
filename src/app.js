599a290d69b47deb2953f573393e9eb5ec74c2af
Alan Enrique Maldonado Navarro <mlgalanmn88@gmail.com>
2020-08-26 23:25:33 -0500

    Updated

--- src/app.js
+++ src/app.js
@@ -16,15 +16,7 @@ class Application {
       console.error("Was not able to obtain the Canvas");
       return;
     } else {
-      console.log("\t> Obtained Canvas");
-    }
-
-    const CP = document.querySelector("#colorP");
-    if (!CP) {
-      console.error("Was not able to obtain the Color Picker");
-      return;
-    } else {
-      console.log("\t> Obtained Color Picker");
+      // console.log("\t> Obtained Canvas");
     }
 
     const webGLInstance = getWebGLContext(canvas);
@@ -32,7 +24,7 @@ class Application {
       console.error("Was not able to obtain the WebGL Instance");
       return;
     } else {
-      console.log("\t> Obtained WebGL Instance");
+      // console.log("\t> Obtained WebGL Instance");
     }
 
     this.canvas = canvas;
@@ -47,21 +39,21 @@ class Application {
       console.error("Was not able to obtain the VSHADER");
       return;
     } else {
-      console.log("\t> Obtained VSHADER");
+      // console.log("\t> Obtained VSHADER");
     }
 
     if (!this.SHADERS.FSHADER) {
       console.error("Was not able to obtain the FSHADER");
       return;
     } else {
-      console.log("\t> Obtained FSHADER");
+      // console.log("\t> Obtained FSHADER");
     }
 
     if (!initShaders(this.gl, this.SHADERS.VSHADER, this.SHADERS.FSHADER)) {
       console.error("Failed to initialize shaders");
       return;
     } else {
-      console.log("\t> Initialized Shaders");
+      // console.log("\t> Initialized Shaders");
     }
 
     console.log("= Setup Complete!");
@@ -70,8 +62,11 @@ class Application {
 
   main = () => {
     /** Setup Controller */
+    this._setup_controls();
     const GameControlMaster = new Controls(this);
     /** Setup Init Render */
+
+    /** End Params */
     this.gl.enable(this.gl.DEPTH_TEST);
     this.gl.depthFunc(this.gl.LESS);
     this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -80,73 +75,63 @@ class Application {
   };
 
   render = () => {};
+
+  _setup_controls = () => {
+    this.CREATION_OBJ_INSTANCE = null;
+    this.canvas.addEventListener("mousedown", (e) => {
+      if (GlobalStorage.CurrentMode === CREATIONSTATE) {
+        this.CREATION_OBJ_INSTANCE = new CustomObject();
+      }
+
+      switch (e.button) {
+        case 0: //Left Click
+          this._leftClick(e);
+          break;
+        case 2: //Right Click
+          e.preventDefault();
+
+          if (GlobalStorage.CurrentMode === CREATIONSTATE) {
+            console.log("Right Click");
+            break;
+          }
+          break;
+        default:
+          break;
+      }
+    });
+
+    document.addEventListener("keydown", (e) => {
+      switch (event.keyCode) {
+        case 38: //A_Up
+          if (GlobalStorage.CurrentMode === CREATIONSTATE) {
+            GlobalStorage.CREATION_DATA.z_index++;
+            console.log("Z++");
+            break;
+          }
+          break;
+        case 40: //A_Down
+          if (GlobalStorage.CurrentMode === CREATIONSTATE) {
+            GlobalStorage.CREATION_DATA.z_index--;
+            console.log("Z--");
+            break;
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  };
+
+  _leftClick = (ev) => {
+    if (GlobalStorage.CurrentMode === CREATIONSTATE) {
+      console.log("Left Click");
+      this.CREATION_OBJ_INSTANCE.pushVertex();
+      break;
+    }
+  };
 }
 
 const Start = () => {
   const App = new Application();
   App.Start();
-};
-
-const initVertexBuffers = (gl, vertices, colors) => {
-  var n = vertices.length / 3;
-  var vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
-
-  var a_Position = gl.getAttribLocation(gl.program, "a_Position");
-  if (a_Position < 0) {
-    console.log("Failed to get program for a_Position");
-    return;
-  }
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  // var radian = (delta * Math.PI) / 180.0;
-  // var cosB = Math.cos(radian);
-  // var sinB = Math.sin(radian);
-
-  // var transformMatrix = new Float32Array([
-  //   cosB,
-  //   -sinB,
-  //   0,
-  //   0,
-  //   sinB,
-  //   cosB,
-  //   0,
-  //   0,
-  //   0,
-  //   0,
-  //   1,
-  //   0,
-  //   0,
-  //   0,
-  //   0,
-  //   1,
-  // ]);
-  // var u_TransformMatrix = gl.getUniformLocation(
-  //   gl.program,
-  //   "u_TransformMatrix"
-  // );
-  // if (!u_TransformMatrix) {
-  //   console.log("Failed to get location for u_TransformMatrix");
-  //   return;
-  // }
-  // gl.uniformMatrix4fv(u_TransformMatrix, false, transformMatrix);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  var colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.DYNAMIC_DRAW);
-
-  var a_Color = gl.getAttribLocation(gl.program, "a_Color");
-  if (a_Color < 0) {
-    console.log("Failed to get location of a_Color");
-    return;
-  }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Color);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LESS);
-  return n;
 };
